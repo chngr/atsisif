15d0b9e62d19d52d3750bc708f077b39b2d260cd
Raymond Cheng <chengraymond@live.com>
2015-03-17 21:37:30 -0400

    Refactor code a bit.

--- README.md
+++ README.md
@@ -6,14 +6,14 @@ redder edges have weight close to 0.5 and bluer edges have weight close to 0.
 
 There are several interactions that are currently possible:
   - A slider allows one to hide edges of weights higher than some value;
-  - The '''h''' key toggles a selection mode which allows one to add nodes into
+  - The ```h``` key toggles a selection mode which allows one to add nodes into
     a "handle" set;
-  - The '''a''' key adds a "tooth";
-  - The '''t''' key toggles a selection mode which allows one to add nodes into
+  - The ```a``` key adds a "tooth";
+  - The ```t``` key toggles a selection mode which allows one to add nodes into
     the current "tooth";
-  - The '''n''' key shifts the tooth selection up one;
-  - The '''b''' key shifts the tooth selection down one;
-  - The '''c''' key clears all the selections so far.
+  - The ```n``` key shifts the tooth selection up one;
+  - The ```b``` key shifts the tooth selection down one;
+  - The ```c``` key clears all the selections so far.
 No restrictions are currently in place making sure that the handle sets and the
 teeth are valid. In particular, nothing checks that a selection is actually a
 comb.
--- src/atsisif.js
+++ src/atsisif.js
@@ -1,19 +1,104 @@
-var KEYCODE = {'h': 72, 'c': 67, 't': 84, 'n': 78, 'b': 66, 'd': 68, 'a': 65};
+var KEYCODE = {'h': 72, 'c': 67, 't': 84, 'n': 78, 'b': 66, 'd': 68, 'a': 65},
+    TOGGLES = {'handle': false, 'tooth': false};
+
+function toggle(t) {
+  var toggle_keys = Object.keys(TOGGLES), key, i;
+  for(i in toggle_keys) {
+    key = toggle_keys[i];
+    TOGGLES[key] = key != t ? false : !TOGGLES[key];
+  }
+}
+
+function update_display(comb, edges) {
+  for(var i = 0; i < comb.teeth.length; i++) {
+    d3.select("#tooth_" + i).text(comb.teeth[i]);
+  }
+  d3.select("#handle_nodes").text(comb.handle);
+  var comb_sum, rhs = 3 * comb.teeth.length + 1;
+  if(edges) {
+    comb_sum = get_delta_weight(comb.handle, edges) +
+      comb.teeth.reduce(function(acc, x) {
+        return acc + get_delta_weight(x, edges);
+      }, 0);
+  }
+  d3.select("#comb_weight_sum").text(comb_sum);
+  d3.select("#comb_difference").text(rhs - comb_sum)
+}
+
+function tooth_colours(comb) {
+  d3.select("#tooth_" + comb.prev_t + "_label")
+    .style("background", "white");
+  d3.select("#tooth_" + comb.cur_t + "_label")
+    .style("background", "yellow");
+}
+
+function reset_comb(comb, vis) {
+  for(var i = 0; i < comb.teeth.length; i++) {
+    d3.select("#tooth_" + i + "_container").remove();
+  }
+  comb.cur_t = comb.prev_t = -1;
+  comb.teeth = comb.handle = [];
+  update_display(comb);
+  vis.selectAll('.handle').style("fill", "red").classed('handle', false);
+  vis.selectAll('.tooth').style("stroke", "black").classed("tooth", false);
+}
+
+function add_tooth(comb, t_cols) {
+  comb.prev_t = comb.cur_t;
+  comb.cur_t += 1;
+  comb.teeth.push([]);
+
+  var new_name = "tooth_" + comb.cur_t + ": ",
+      new_elt = d3.select("#teeth")
+                  .append("p")
+                  .attr("id", "tooth_" + comb.cur_t + "_container");
+  new_elt.append("span")
+    .attr("id", "tooth_" + comb.cur_t + "_label")
+    .style("color", t_cols(comb.cur_t))
+    .text(new_name);
+  new_elt.append('span').attr('id', "tooth_" + comb.cur_t);
+  tooth_colours(comb);
+}
+function next_tooth(comb) {
+  comb.prev_t = comb.cur_t;
+  comb.cur_t = comb.cur_t < comb.teeth.length ? comb.cur_t + 1 : comb.cur_t;
+  tooth_colours(comb);
+  console.log("Current tooth: " + comb.cur_t);
+}
+function back_tooth(comb) {
+  comb.prev_t = comb.cur_t;
+  comb.cur_t = comb.cur_t > 0 ? comb.cur_t - 1 : comb.cur_t;
+  tooth_colours(comb.prev_t);
+  console.log("Current tooth: " + comb.cur_t);
+}
+function get_delta_weight(V, edges) {
+  return edges.reduce(function(a, e) {
+    return (V.indexOf(e.a) > -1) + (V.indexOf(e.b) > -1) == 1 ? a + e.weight : a;
+  }, 0);
+}
 
 function draw_graph() {
-  var width = 1000,
-      height = 600,
+  var width = 1000, height = 600,
       edges = [],
-      toggles = {'handle': false, 'tooth': false},
-      handle = [],
-      previous_tooth = -1,
-      current_tooth = -1,
-      teeth = [];
+      comb = {
+        handle: [],
+        teeth: [],
+        cur_t: -1,
+        prev_t: -1,
+      };
 
-  var colour = d3.scale.linear().domain([0.0,0.5,1.0]).range(["cornflowerblue", "red", "black"]),
-      teeth_colours = d3.scale.linear().domain([0,5,10,15]).range(["green", "magenta", "yellow", "orange"]),
-      xScale = d3.scale.linear().domain([0,width]).range([0,width]),
-      yScale = d3.scale.linear().domain([0,height]).range([0,height]);
+  var colour = d3.scale.linear()
+                  .domain([0.0,0.5,1.0])
+                  .range(["cornflowerblue", "red", "black"]),
+      t_cols = d3.scale.linear()
+                  .domain([0,5,10,15])
+                  .range(["green", "magenta", "yellow", "orange"]),
+      xScale = d3.scale.linear()
+                  .domain([0,width])
+                  .range([0,width]),
+      yScale = d3.scale.linear()
+                  .domain([0,height])
+                  .range([0,height]);
 
   var force = d3.layout.force()
                 .charge(-10)
@@ -33,34 +118,27 @@ function draw_graph() {
                 .y(yScale)
                 .on("zoom", zoom);
 
-  function zoom() {
-    svg.attr("transform", "translate("+ d3.event.translate +")scale("+ d3.event.scale +")");
-  }
-
-  var svg_graph = svg.append("svg:g").call(zoomer);
-  var rect = svg_graph.append("svg:rect")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("fill", "transparent")
-    .attr("stroke", "transparent")
-    .attr("stroke-width", 1)
-    .attr("id", "zrect");
-  var vis = svg_graph.append("svg:g");
-  vis.attr("fill", "red")
-     .attr("stroke", "black")
-     .attr("stroke-width", 1)
-     .attr("opacity", 0.5)
-     .attr("id", "vis");
-
-  var link = vis.append("g").attr("class", "link").selectAll("line");
-  var node = vis.append("g").attr("class", "node").selectAll("circle");
+  var svg_graph = svg.append("svg:g").call(zoomer),
+      rect = svg_graph.append("svg:rect")
+              .attr("width", width)
+              .attr("height", height)
+              .attr("fill", "transparent")
+              .attr("stroke", "transparent")
+              .attr("stroke-width", 1)
+              .attr("id", "zrect"),
+      vis = svg_graph.append("svg:g")
+              .attr("fill", "red")
+              .attr("stroke", "black")
+              .attr("stroke-width", 1)
+              .attr("opacity", 0.5)
+              .attr("id", "vis");
+  var link = vis.append("g").attr("class", "link").selectAll("line"),
+      node = vis.append("g").attr("class", "node").selectAll("circle");
 
   d3.json("/data/c.json", function(err, g) {
-    var n = g.nodes;
-    var nodes = [];
-    for(var i = 0; i < n; i++) {
-      nodes.push({name: i});
-    }
+    var n = g.nodes, nodes = [];
+    for(var i = 0; i < n; i++) { nodes.push({name: i}); }
+
     force.nodes(nodes).links(g.edges).start();
     edges = force.links().map(function(e) {
       return {a: e.source.index, b: e.target.index, weight: e.value };
@@ -75,44 +153,7 @@ function draw_graph() {
                .attr("class", "node")
                .attr("r", 2)
                .call(force.drag)
-               .on("click", function(d) {
-                if(d3.event.defaultPrevented) return;
-                if(toggles.handle) {
-                  d3.select(this).classed("handle", d.handle = !d.handle)
-                    .style("fill", function(p) {
-                      if(p.handle) {
-                        return 'blue';
-                      } else {
-                        return 'red';
-                      }
-                    });
-                  if(d.handle) {
-                    handle.push(d.index);
-                  } else {
-                    handle.splice(handle.indexOf(d.index), 1);
-                  }
-                } else if(toggles.tooth) {
-                  d3.select(this).classed("tooth", d.tooth = !d.tooth)
-                    .style("stroke", function(p) {
-                      if(p.tooth) {
-                        return teeth_colours(current_tooth);
-                      } else {
-                        return "black";
-                      }
-                    });
-                  if(d.tooth) {
-                    teeth[current_tooth].push(d.index);
-                  } else {
-                    for(var i = 0; i < teeth.length; i++) {
-                      var idx = teeth[i].indexOf(d.index);
-                      if(idx > -1) {
-                        teeth[i].splice(idx, 1);
-                      }
-                    }
-                  }
-                }
-                update_display();
-               });
+               .on("click", node_click);
 
     force.on("tick", function() {
       link.attr("x1", function(d) { return d.source.x; })
@@ -124,36 +165,18 @@ function draw_graph() {
     });
   });
 
-  function update_display() {
-    for(var i = 0; i < teeth.length; i++) {
-      d3.select("#tooth_" + i).text(teeth[i]);
-    }
-    d3.select("#handle_nodes").text(handle);
-    var comb_sum = get_delta_weight(handle) +
-      teeth.reduce(function(acc, x) {
-        return acc + get_delta_weight(x);
-      }, 0),
-      rhs = 3 * teeth.length + 1;
-    d3.select("#comb_weight_sum").text(comb_sum);
-    d3.select("#comb_difference").text(rhs - comb_sum)
-  }
-  d3.select("#weight").on("input", function() { update(this.value); });
-  update(100);
-  function update(wt) {
-    d3.select("#weight").property("value", wt);
-    link.style("stroke-opacity", function(d) {
-      if(100 * d.value <= wt) {
-        return 1;
-      } else {
-        return 0;
-      }
-    });
-  };
 
+  d3.select("#weight").on("input", function() {
+    var wt = this.value;
+    d3.select("#weight").property("value", wt);
+    link.style("stroke-opacity", function(d) { return 100 * d.value <= wt ? 1 : 0; })
+  });
+
+  /* Event handlers */
   function keydown() {
     switch(d3.event.keyCode) {
       case KEYCODE.c:
-        clear();
+        reset_comb(comb, vis);
         break;
       case KEYCODE.h:
         toggle('handle');
@@ -162,82 +185,47 @@ function draw_graph() {
         toggle('tooth');
         break;
       case KEYCODE.n:
-        next_tooth();
+        next_tooth(comb);
         break;
       case KEYCODE.b:
-        back_tooth();
+        back_tooth(comb);
         break;
       case KEYCODE.a:
-        add_tooth();
+        add_tooth(comb, t_cols);
         break;
     }
   }
-  function clear() {
-    for(var i = 0; i < teeth.length; i++) {
-      d3.select("#tooth_" + i + "_container").remove();
-    }
-    current_tooth = -1;
-    previous_tooth = -1;
-    teeth = [];
-    handle = [];
-    update_display();
-    vis.selectAll('.handle').style("fill", "red").classed('handle', false);
-    vis.selectAll('.tooth').style("stroke", "black").classed("tooth", false);
-  }
-  function toggle(t) {
-    var toggle_keys = Object.keys(toggles);
-    for(var i in toggle_keys) {
-      var key = toggle_keys[i];
-      if(key != t) {
-        toggles[key] = false;
-      }
-    }
-    toggles[t] = !toggles[t];
+
+  function zoom() {
+    svg.attr("transform", "translate("+ d3.event.translate +")scale("+ d3.event.scale +")");
   }
 
-  function add_tooth() {
-    previous_tooth = current_tooth;
-    current_tooth += 1;
-    teeth.push([]);
+  function node_click(d) {
+    if(d3.event.defaultPrevented) return;
+    if(TOGGLES.handle) {
+      d3.select(this)
+        .classed("handle", d.handle = !d.handle)
+        .style("fill", function(p) { return (p.handle ? "blue" : "red"); });
 
-    var new_tooth_name = "tooth_" + current_tooth + ": ",
-        new_tooth_elem = d3.select("#teeth").append("p").attr("id", "tooth_" + current_tooth + "_container");
-    new_tooth_elem.append("span")
-      .attr("id", "tooth_" + current_tooth + "_label")
-      .style("color", teeth_colours(current_tooth))
-      .text(new_tooth_name);
-    new_tooth_elem.append('span').attr('id', "tooth_" + current_tooth);
-    tooth_colours(previous_tooth, current_tooth);
-  }
-  function next_tooth() {
-    previous_tooth = current_tooth;
-    if(current_tooth < teeth.length) {
-      current_tooth += 1;
-    }
-    tooth_colours(previous_tooth, current_tooth);
-    console.log("Current tooth: " + current_tooth);
-  }
-  function back_tooth() {
-    previous_tooth = current_tooth;
-    if(current_tooth > 0) {
-      current_tooth -= 1;
-    }
-    tooth_colours(previous_tooth, current_tooth);
-    console.log("Current tooth: " + current_tooth);
-  }
-  function tooth_colours(prev_t, cur_t) {
-    d3.select("#tooth_" + prev_t + "_label")
-      .style("background", "white");
-    d3.select("#tooth_" + cur_t + "_label")
-      .style("background", "yellow");
-  }
-  function get_delta_weight(V) {
-    return edges.reduce(function(acc, e) {
-      if((V.indexOf(e.a) > -1) + (V.indexOf(e.b) > -1) == 1) {
-        return acc + e.weight;
+      if(d.handle) {
+        comb.handle.push(d.index);
       } else {
-        return acc;
+        comb.handle.splice(comb.handle.indexOf(d.index), 1);
       }
-    }, 0);
-  }
+    } else if(TOGGLES.tooth) {
+      d3.select(this).classed("tooth", d.tooth = !d.tooth)
+        .style("stroke", function(p) { return (p.tooth ? t_cols(comb.cur_t) : "black"); });
+      if(d.tooth) {
+        comb.teeth[comb.cur_t].push(d.index);
+      } else {
+        for(var i = 0; i < comb.teeth.length; i++) {
+          var idx = comb.teeth[i].indexOf(d.index);
+          if(idx > -1) {
+            comb.teeth[i].splice(idx, 1);
+          }
+        }
+      }
+    }
+    update_display(comb, edges);
+   };
 }
