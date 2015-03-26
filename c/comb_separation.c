70d8195074cf985c9f2cc99c53e22aafbbf4b8d6
Raymond Cheng <chengraymond@live.com>
2015-03-26 09:34:05 -0400

    Clean up main file.

--- c/comb_separation.c
+++ c/comb_separation.c
@@ -5,188 +5,109 @@
 #include "graph.h"
 #include "util.h"
 
-static int euclid_edgelen (int i, int j, double *x, double *y);
-static int getprob (char *fname, int *p_ncount, int *p_ecount, int **p_elist, int **p_elen);
+static int getprob (char *fname, int *p_ncount, int *p_ecount, int **p_elist, double **p_ewt);
 static int parseargs (int ac, char **av);
 static void usage (char *f);
 
 static char *fname = (char *) NULL;
 static int seed = 0;
-static int geometric_data = 0;
-static int ncount_rand = 0;
-static int gridsize_rand = 100;
 
 int main (int ac, char **av)
 {
-    int rval  = 0, ncount = 0, ecount = 0;
-    int *elist = (int *) NULL, *elen = (int *) NULL, *tlist = (int *) NULL;
-    double szeit;
+  int rval  = 0, ncount = 0, ecount = 0;
+  int *elist = NULL, *tlist = NULL;
+  double *ewt = NULL;
+  double szeit;
 
-    seed = (int) CO759_real_zeit ();
+  seed = (int) CO759_real_zeit ();
 
-    rval = parseargs (ac, av);
-    if (rval) goto CLEANUP;
+  rval = parseargs (ac, av);
+  if (rval) goto CLEANUP;
 
-    if (!fname && !ncount_rand) {
-        printf ("Must specify a problem file or use -k for random prob\n");
-        rval = 1; goto CLEANUP;
-    }
-    printf ("Seed = %d\n", seed);
-    srandom (seed);
+  if (!fname) {
+    printf ("Must specify a problem file\n");
+    rval = 1; goto CLEANUP;
+  }
+  printf ("Seed = %d\n", seed);
+  srandom (seed);
 
-    if (fname) {
-        printf ("Problem name: %s\n", fname);
-        if (geometric_data) printf ("Geometric data\n");
-    }
+  if (fname) printf ("Problem name: %s\n", fname);
 
-    rval = getprob (fname, &ncount, &ecount, &elist, &elen);
-    if (rval) {
-        fprintf (stderr, "getprob failed\n"); goto CLEANUP;
-    }
+  rval = getprob (fname, &ncount, &ecount, &elist, &ewt);
+  if (rval) { fprintf (stderr, "getprob failed\n"); goto CLEANUP; }
 
-    tlist = (int *) malloc ((ncount)*sizeof (int));
-    if (!tlist) {
-        fprintf (stderr, "out of memory for tlist\n");
-        rval = 1;  goto CLEANUP;
-    }
+  tlist = malloc ( ncount * sizeof (int));
+  if (!tlist) {
+      fprintf (stderr, "out of memory for tlist\n");
+      rval = 1;  goto CLEANUP;
+  }
 
-    szeit = CO759_zeit ();
-    printf ("Running Time: %.2f seconds\n", CO759_zeit() - szeit);
-    fflush (stdout);
+  szeit = CO759_zeit ();
+  printf ("Running Time: %.2f seconds\n", CO759_zeit() - szeit);
+  fflush (stdout);
 
 CLEANUP:
-
-    if (tlist) free (tlist);
-    if (elist) free (elist);
-    if (elen) free (elen);
-    return rval;
+  if (tlist) free (tlist);
+  if (elist) free (elist);
+  if (ewt) free (ewt);
+  return rval;
 }
 
 static int getprob (char *filename, int *p_ncount, int *p_ecount, int **p_elist,
-    int **p_elen)
+    double **p_ewt)
 {
-    FILE *f = (FILE *) NULL;
-    int i, j, end1, end2, w, rval = 0, ncount, ecount;
-    int *elist = (int *) NULL, *elen = (int *) NULL;
-    double *x = (double *) NULL, *y = (double *) NULL;
+  FILE *f = NULL;
+  int i, end1, end2, w, rval = 0, ncount, ecount;
+  int *elist = NULL;
+  double *ewt = NULL;
 
-    if (filename) {
-        if ((f = fopen (filename, "r")) == NULL) {
-    	    fprintf (stderr, "Unable to open %s for input\n",filename);
-            rval = 1;  goto CLEANUP;
-        }
+  if (filename) {
+    if ((f = fopen (filename, "r")) == NULL) {
+      fprintf (stderr, "Unable to open %s for input\n",filename);
+      rval = 1;  goto CLEANUP;
     }
 
-    if (filename && geometric_data == 0) {
-        if (fscanf (f, "%d %d", &ncount, &ecount) != 2) {
-       	    fprintf (stderr, "Input file %s has invalid format\n",filename);
-            rval = 1;  goto CLEANUP;
-        }
-
-        printf ("Nodes: %d  Edges: %d\n", ncount, ecount);
-        fflush (stdout);
-
-        elist = (int *) malloc (2 * ecount * sizeof (int));
-        if (!elist) {
-            fprintf (stderr, "out of memory for elist\n");
-            rval = 1;  goto CLEANUP;
-        }
-
-        elen = (int *) malloc (ecount * sizeof (int));
-        if (!elen) {
-            fprintf (stderr, "out of memory for elen\n");
-            rval = 1;  goto CLEANUP;
-        }
-
-        for (i = 0; i < ecount; i++) {
-    	    if (fscanf(f,"%d %d %d",&end1, &end2, &w) != 3) {
-	        fprintf (stderr, "%s has invalid input format\n", filename);
-                rval = 1;  goto CLEANUP;
-	    }
-	    elist[2*i] = end1;
-	    elist[2*i+1] = end2;
-	    elen[i] = w;
-        }
-    } else {
-        if (filename) {
-            if (fscanf (f, "%d", &ncount) != 1) {
-       	        fprintf (stderr, "Input file %s has invalid format\n",filename);
-                rval = 1;  goto CLEANUP;
-            }
-        } else {
-            ncount = ncount_rand;
-        }
-
-        x = (double *) malloc (ncount * sizeof (double));
-        y = (double *) malloc (ncount * sizeof (double));
-        if (!x || !y) {
-            fprintf (stdout, "out of memory for x or y\n");
-            rval = 1; goto CLEANUP;
-        }
-
-        if (filename) {
-            for (i = 0; i < ncount; i++) {
-    	        if (fscanf(f,"%lf %lf",&x[i], &y[i]) != 2) {
-	            fprintf (stderr, "%s has invalid input format\n", filename);
-                    rval = 1;  goto CLEANUP;
-	        }
-            }
-        } else {
-            rval = CO759_build_xy (ncount, x, y, gridsize_rand);
-            if (rval) {
-                fprintf (stderr, "CO759_build_xy failed\n");
-                goto CLEANUP;
-            }
-
-            printf ("%d\n", ncount);
-            for (i = 0; i < ncount; i++) {
-                printf ("%.0f %.0f\n", x[i], y[i]);
-            }
-            printf ("\n");
-        }
-
-        ecount = (ncount * (ncount - 1)) / 2;
-        printf ("Complete graph: %d nodes, %d edges\n", ncount, ecount);
-
-        elist = (int *) malloc (2 * ecount * sizeof (int));
-        if (!elist) {
-            fprintf (stderr, "out of memory for elist\n");
-            rval = 1;  goto CLEANUP;
-        }
-
-        elen = (int *) malloc (ecount * sizeof (int));
-        if (!elen) {
-            fprintf (stderr, "out of memory for elen\n");
-            rval = 1;  goto CLEANUP;
-        }
-
-        ecount = 0;
-        for (i = 0; i < ncount; i++) {
-            for (j = i+1; j < ncount; j++) {
-                elist[2*ecount] = i;
-                elist[2*ecount+1] = j;
-                elen[ecount] = euclid_edgelen (i, j, x, y);
-                ecount++;
-            }
-        }
+    if (fscanf (f, "%d %d", &ncount, &ecount) != 2) {
+      fprintf (stderr, "Input file %s has invalid format\n",filename);
+      rval = 1;  goto CLEANUP;
     }
 
-    *p_ncount = ncount;
-    *p_ecount = ecount;
-    *p_elist = elist;
-    *p_elen = elen;
+    printf ("Nodes: %d  Edges: %d\n", ncount, ecount);
+    fflush (stdout);
+
+    elist = (int *) malloc (2 * ecount * sizeof (int));
+    if (!elist) {
+      fprintf (stderr, "out of memory for elist\n");
+      rval = 1;  goto CLEANUP;
+    }
+
+    ewt = malloc (ecount * sizeof (double));
+    if (!ewt) {
+      fprintf (stderr, "out of memory for elen\n");
+      rval = 1;  goto CLEANUP;
+    }
+
+    for (i = 0; i < ecount; i++) {
+      if (fscanf(f,"%d %d %d",&end1, &end2, &w) != 3) {
+      fprintf (stderr, "%s has invalid input format\n", filename);
+      rval = 1;  goto CLEANUP;
+    }
+    elist[2*i] = end1;
+    elist[2*i+1] = end2;
+    ewt[i] = w;
+    }
+  } else {
+    fprintf(stderr, "A filename is required\n");
+    rval = 1; goto CLEANUP;
+  }
+  *p_ncount = ncount;
+  *p_ecount = ecount;
+  *p_elist = elist;
+  *p_ewt = ewt;
 
 CLEANUP:
-    if (f) fclose (f);
-    if (x) free (x);
-    if (y) free (y);
-    return rval;
-}
-
-static int euclid_edgelen (int i, int j, double *x, double *y)
-{
-    double t1 = x[i] - x[j], t2 = y[i] - y[j];
-    return (int) (sqrt (t1 * t1 + t2 * t2) + 0.5);
+  if (f) fclose (f);
+  return rval;
 }
 
 static int parseargs (int ac, char **av)
@@ -200,15 +121,6 @@ static int parseargs (int ac, char **av)
 
     while ((c = getopt (ac, av, "ab:gk:s:")) != EOF) {
         switch (c) {
-        case 'b':
-            gridsize_rand = atoi (optarg);
-            break;
-        case 'g':
-            geometric_data = 1;
-            break;
-        case 'k':
-            ncount_rand = atoi (optarg);
-            break;
         case 's':
             seed = atoi (optarg);
             break;
@@ -232,10 +144,5 @@ static int parseargs (int ac, char **av)
 static void usage (char *f)
 {
     fprintf (stderr, "Usage: %s [-see below-] [prob_file]\n", f);
-    fprintf (stderr, "   -a    add all subtours cuts at once\n");
-    fprintf (stderr, "   -b d  gridsize d for random problems\n");
-    fprintf (stderr, "   -g    prob_file has x-y coordinates\n");
-    fprintf (stderr, "   -k d  generate problem with d cities\n");
     fprintf (stderr, "   -s d  random seed\n");
 }
-
